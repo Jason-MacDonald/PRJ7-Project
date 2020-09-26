aaf08503bcac136b0ee6e5f6684f0e8b575a95f9
Jason-MacDonald <49774612+Jason-MacDonald@users.noreply.github.com>
2020-09-26 18:36:57 +1200

    Issue

---
+++ adventure-platform/adventure-platform/Shared/DTOs/Alert.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adventureplatform.Shared.DTOs
+{
+    public class Alert
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+    }
+}
